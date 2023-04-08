6b82c5116bbf5811ac373d7f7e9bcf6171997eb4
Eric Rochester <erochest@gmail.com>
2023-04-08 12:15:50 -0400

    edge case around starting after the page count

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,21 @@
 pub mod error;
 
+pub fn get_pages(start_page: u32, page_count: u32) -> impl Iterator<Item = u32> {
+    (start_page..page_count).into_iter()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::error::Result;
+    use pretty_assertions::assert_eq;
 
     #[test]
-    fn it_works() -> Result<()> {
-        Ok(())
+    fn when_starts_past_page_count_then_returns_empty_iter() {
+        let page_count = 10;
+        let start_page = 11;
+
+        let result = get_pages(start_page, page_count);
+
+        assert_eq!(result.count(), 0);
     }
 }
